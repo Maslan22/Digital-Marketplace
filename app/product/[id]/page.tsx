323d0343b0469e77e06b1e2eff1e36ca15f12860
HENRY IDDRISU MASLAN <henry.maslan@innorik.com>
2024-06-14 14:16:27 +0000

    Finished caching

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/carousel";
 import { JSONContent } from "@tiptap/react";
 import Image from "next/image";
+import { unstable_noStore as noStore } from "next/cache";
 
 async function getData(id: string) {
   const data = await prisma.product.findUnique({
@@ -44,6 +45,7 @@ export default async function ProductPage({
 }: {
   params: { id: string };
 }) {
+  noStore();
   const data = await getData(params.id);
   return (
     <section className="max-w-7xl mx-auto px-4 lg:px-8 lg:grid lg:grid-rows-1 lg:grid-cols-7 lg:gap-x-8 lg:gap-y-10 xl:gap-x-16">
